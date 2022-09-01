e03d2adb3fe90e1a31c44ab6109cf8fa29d76b1b
EC2 Default User <ec2-user@ip-172-31-45-234.us-east-2.compute.internal>
2022-09-01 19:10:50 +0000

    added output values

--- service-templates/template1/micro-app/v1/instance_infrastructure/main.tf
+++ service-templates/template1/micro-app/v1/instance_infrastructure/main.tf
@@ -1,7 +1,3 @@
-locals {
-  local_data = jsondecode(file("${path.module}/proton.auto.tfvars.json"))
-}
-
 resource "kubernetes_namespace" "namespace" {
   metadata {
     name = var.service_instance.inputs.namespace_name
--- service-templates/template1/micro-app/v1/instance_infrastructure/versions.tf
+++ service-templates/template1/micro-app/v1/instance_infrastructure/versions.tf
@@ -12,21 +12,21 @@ terraform {
   required_version = "~>1.0"
 }
 
-locals {
+/*locals {
   local_data = jsondecode(file("../proton.auto.tfvars.json"))
-}
+}*/
 
 provider "aws" {
-  region  = local.local_data.environment.inputs.cluster_region
+  region  = var.environment.outputs.cluster_region
 }
 
 
 data "aws_eks_cluster" "cluster" {
-  name =local.local_data.environment.inputs.cluster_name
+  name =var.environment.outputs.cluster_name
 }
 
 provider "kubernetes" {
-  host                   = local.local_data.environment.outputs.endpoint
+  host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
@@ -35,7 +35,7 @@ provider "kubernetes" {
       "eks",
       "get-token",
       "--cluster-name",
-      local.local_data.environment.outputs.cluster_name
+      data.aws_eks_cluster.cluster.name
     ]
   }
 }
