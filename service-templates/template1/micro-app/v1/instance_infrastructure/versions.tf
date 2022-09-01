c6a4b66a8b643329518efe080e8a1019ab15f525
EC2 Default User <ec2-user@ip-172-31-45-234.us-east-2.compute.internal>
2022-09-01 15:24:13 +0000

    added some changes

--- service-templates/template1/micro-app/v1/instance_infrastructure/versions.tf
+++ service-templates/template1/micro-app/v1/instance_infrastructure/versions.tf
@@ -27,11 +27,11 @@ provider "aws" {
  
 
 data "aws_eks_cluster" "cluster" {
-  name =var.environment.outputs.cluster_id
+  name =var.environment.outputs.cluster_name
 }
 
 provider "kubernetes" {
-  host                   = var.environment.outputs.cluster.endpoint
+  host                   = var.environment.outputs.endpoint
   cluster_ca_certificate = base64decode(var.environment.outputs.kubeconfig_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
