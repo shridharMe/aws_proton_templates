de3a5269d401a54caf4a25a51da8436077971b2f
Shridhar <36041359+shridharMe@users.noreply.github.com>
2022-09-01 17:34:08 +0100

    Update proton.variables.tf

--- service-templates/template1/micro-app/dev-resources/proton.variables.tf
+++ service-templates/template1/micro-app/dev-resources/proton.variables.tf
@@ -1,1 +1,15 @@
 
+variable "service_instance" {
+  type = object({
+    name   = string
+    inputs = map(string)
+  })
+  default = null
+}
+
+variable "environment" {
+  type = object({
+    outputs = map(string)
+  })
+  default = null
+}
