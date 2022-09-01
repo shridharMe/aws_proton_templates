909976eddb00aec7405a2fc4cd1bb113e3d6b9ff
EC2 Default User <ec2-user@ip-172-31-45-234.us-east-2.compute.internal>
2022-09-01 20:40:01 +0000

    rediced the noise

--- environment-templates/template3/eks/v1/infrastructure/outputs.tf
+++ environment-templates/template3/eks/v1/infrastructure/outputs.tf
@@ -1,16 +1,3 @@
-output "endpoint" {
-  value = data.aws_eks_cluster.cluster.endpoint
-}
-
-output "kubeconfig_certificate_authority_data" {
-  value = data.aws_eks_cluster.cluster.certificate_authority[0].data
-}
-
-# Only available on Kubernetes version 1.13 and 1.14 clusters created or upgraded on or after September 3, 2019.
-output "identity-oidc-issuer" {
-  value = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
-}
-
 output "cluster_name" {
   value = data.aws_eks_cluster.cluster.name
 }
